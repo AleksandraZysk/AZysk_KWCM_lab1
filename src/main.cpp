3915ca9ae1a3fbcad1d97e9836bde192240c8578
Student <alekzys437@student.polsl.pl>
2018-02-23 13:56:09 +0100

    Drugi commit - czesc druga

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "trygonometria.h"
+
+int main() {
+	double a = degreemath::sin(90);
+	double b = degreemath::cos(0);
+	double c = degreemath::tg(0);
+	double d = degreemath::ctg(90);
+	return 0;
+}	
